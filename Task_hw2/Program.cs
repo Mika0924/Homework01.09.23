8d91034a135f19b7b4d0db494600bd620646f95a
mika <mika.mnurmasheva.mika@mail.ru>
2023-09-01 21:31:50 +0600

    Task02

---
+++ Task_hw2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int i = int.Parse(ReadInput);
+    return i;
+}
+int General(int n)
+{
+    int i = 0;
+    while (n > 0)
+    {
+        i += n % 10;
+        n = n / 10;
+    }
+    return i;
+}
+int n = Prompt("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в цифре {n} = {General(n)}");
