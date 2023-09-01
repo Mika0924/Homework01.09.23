a614ac8f1d7b301c7d118c0986920fce53eb78aa
mika <mika.mnurmasheva.mika@mail.ru>
2023-09-01 21:36:01 +0600

    Task03

---
+++ Task_hw3/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int i = int.Parse(ReadInput);
+    return i;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt ("Введите основание ");
+int exponent = Prompt ("введите показатель ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
